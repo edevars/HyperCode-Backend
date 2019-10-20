3f9d37b5e54fb451af05c88b66ced7b26763f7e0
edevars <e.devars.dev@gmail.com>
2019-10-20 15:27:30 -0500

    :sparkles: Añadiendo comentarios al archivo de configuracion

--- config/index.js
+++ config/index.js
@@ -1,5 +1,11 @@
+//Dotenv es un paquete que nos ayuda a configurar nuestras variables de entorno
 require('dotenv');
 
+//El objeto config nos ayudara a definir todas las variables de entorno
+//necesarias para configurar nuestro servidor
+
+//Es bueno crear este archivo pues si queremos hacer un cambio en toda nuestra
+//apliacion, solo debemos de hacerlo en este archivo
 const config = {
   dev: process.env.NODE_ENV != 'production',
   port: process.env.PORT || 3000
