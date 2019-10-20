61e03a849cf9b88059dad81cb73fb87c7923a405
edevars <e.devars.dev@gmail.com>
2019-10-20 16:42:29 -0500

    :sparkles: Creando rutas en express

--- README.md
+++ README.md
@@ -1,22 +1,112 @@
 # Creando una API Restful en express
 
-Cuando hacemos aplicaciones web estamos acostumbrados a pensar unicamente en el frontend, es decir, el HTML y CSS. Sin embargo el encargado de desarrollar toda la lógica de negocios, mandar e insertar información a nuestra base de datos, o hacer la autenticación de usuarios es el backend. 
+Cuando hacemos aplicaciones web estamos acostumbrados a pensar unicamente en el frontend, es decir, el HTML y CSS. Sin embargo el encargado de desarrollar toda la lógica de negocios, mandar e insertar información a nuestra base de datos, o hacer la autenticación de usuarios es el backend.
 
 El backend es creado mediante un servidor, en nuestro caso lo haremos con express, lo usual para saber como construir nuestro servidor de servicios web es siguiendo una arquitectura llamada REST.
 
 ![Backend vs frontend](https://www.dropbox.com/s/b4zrc5aik33g2kp/backvsfront.jpeg?raw=1)
 
-
 ## Que es REST?
 
-REST es un estilo de  arquitectura para crear web services, no es un estándar pero es considerada una especificación creada por Roy Fielding, quien es cofundador de Apache Software Foundation.
+REST es un estilo de arquitectura para crear web services, no es un estándar pero es considerada una especificación creada por Roy Fielding, quien es cofundador de Apache Software Foundation.
 
 Para poder usar REST debemos hacer uso de los verbos http (si quieres mas info dale click [aqui](https://developer.mozilla.org/es/docs/Web/HTTP/Methods)) donde cada ruta que nosotros definamos para nuestra api podrá regresar o insertar diferentes valores dependiendo el verbo http que usemos.
 
-| URL        | /api/usuarios           | /api/usuarios:id  |
-| ------------- |:-------------:| -----:|
-| GET      | lista a todos los usuarios dentro de la coleccion | lista solo al usuario que coincida con el 'id' |
-| PUT      | Remplaza en toda la coleccion | Remplaza o crea solo UN usuario en especifico |
-| PATCH      | No hace nada | Actualiza a un usuario en especifico |
-| POST      | Crea un nuevo usuario en la coleccion | No hace nada |
-| DELETE      | Elimina toda la coleccion | Elimina a un usuario en especifico con el 'id' |
+| URL    |                   /api/usuarios                   |                               /api/usuarios:id |
+| ------ | :-----------------------------------------------: | ---------------------------------------------: |
+| GET    | lista a todos los usuarios dentro de la coleccion | lista solo al usuario que coincida con el 'id' |
+| PUT    |           Remplaza en toda la coleccion           |  Remplaza o crea solo UN usuario en especifico |
+| PATCH  |                   No hace nada                    |           Actualiza a un usuario en especifico |
+| POST   |       Crea un nuevo usuario en la coleccion       |                                   No hace nada |
+| DELETE |             Elimina toda la coleccion             | Elimina a un usuario en especifico con el 'id' |
+
+En este proyecto en especifico crearemos un backend para un foro de desarrllo llamado _Hypercode_
+
+## Creando las rutas en nuestra app
+
+Las rutas (tambien llamadas endpoints) son las urls que da nuestro servidor para que otras aplicaciones puedan consumir datos, imaginatelas como una especie de puente que sirve para conectar los datos. Estas rutas pueden ser aplicadas a diferentes verbos http, es decir dependiendo de con que verbo mandemos llamar la ruta, es la accion que realizará.
+
+Para esto crearemos una carpeta llamada **routes**, en la cual agregaremos las rutas de nuestras APIs. Primero empezaremos con el metodo get de usuarios, definido de la siguiente manera:
+
+```javascript
+const express = require('express');
+//Hacemos uso de un archivo de mocks, los mocks son datos falsos
+const { usersMock } = require('../utils/mocks/users');
+
+//Definimos una funcion userApi para poder crear una ruta
+//en nuestra app de express, por eso como parametro tiene app
+
+//Esto lo hacemos para ser dinamicos y establecer que aplicacion de express
+//puede hacer uso de nuestra ruta
+function usersApi(app) {
+  //creamos un router con express
+  const router = express.Router();
+
+  //Definimos la ruta principal donde se consumira nuestra api
+  app.use('/api/users', router);
+
+  //Le decimos al router que haga uso de nuestra ruta principal
+  //cuandp se le haga una peticion GET
+
+  //la ruta que se define es '/' pero hace referencia a '/api/users'
+  router.get('/', async function(req, res, next) {
+    try {
+      //Definimos unos usuarios falsos con nuestro mock
+      //Como las peticiones son asincronas hacemos uso de una promesa
+      //esto hace la simulacion como si nos conectaramos a una base de datos
+      const users = await Promise.resolve(usersMock);
+
+      //Devolvemos un codigo 200 http que siginifica ok
+      res.status(200).json({
+        //devolcemos como datos a nuestros usuarios
+        data: users,
+        //devolvemos un mensaje para el cliente
+        message: 'Usuarios listados'
+      });
+    } catch (error) {
+      //Si ocurre un error lo podemos manejar con el parametro next
+      next(error);
+    }
+  });
+}
+
+module.exports = usersApi;
+```
+
+Cabe aclarar que debemos hacer uso de mocks, los cuales son datos falsos que simulan una base de datos, para crear diferentes mocks dependiendo de tus necesidades puedes usar [mockaroo](https://mockaroo.com/).
+
+Los usuarios de un foro suelen estar modelados de la siguiente manera:
+
+```javascript
+    {
+        id: 1,
+        username: 'uwilmore0',
+        email: 'ekittley0@reference.com',
+        password: 'kHfS7AzpK3'
+    }
+```
+
+Por ultimo para hacer uso de nuestra ruta debemos declarar su uso en nuestro archivo principal index de express:
+
+```javascript
+//importamos express para poder crear nuestro servidor
+const express = require('express');
+
+//Creamos una instancia de express
+const app = express();
+
+//Traemos la configuracion de nuestro proyecto
+//a traves de las variables de entorno
+const { config } = require('./config');
+
+//Importamos nuestra API de usuarios
+const usersApi = require('./routes/users');
+
+//Hacemos uso de la API pasando como parametro
+//nuestra apliacicon de express
+usersApi(app);
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
+```
---
+++ utils/mocks/users.js
@@ -0,0 +1,604 @@
+const usersMock = [
+  {
+    id: 1,
+    username: 'uwilmore0',
+    email: 'ekittley0@reference.com',
+    password: 'kHfS7AzpK3'
+  },
+  {
+    id: 2,
+    username: 'olaverenz1',
+    email: 'kmerigot1@home.pl',
+    password: 'XNOwoj2J'
+  },
+  {
+    id: 3,
+    username: 'vclarabut2',
+    email: 'fphillip2@yolasite.com',
+    password: 'qaJDDBmnf2'
+  },
+  {
+    id: 4,
+    username: 'mscotter3',
+    email: 'jzavittieri3@bizjournals.com',
+    password: 'aleSUztxd'
+  },
+  {
+    id: 5,
+    username: 'ggolley4',
+    email: 'sminty4@ihg.com',
+    password: 'sirvVFV'
+  },
+  {
+    id: 6,
+    username: 'mfairholm5',
+    email: 'asimonite5@mayoclinic.com',
+    password: 'FP31XLW'
+  },
+  {
+    id: 7,
+    username: 'iskelhorn6',
+    email: 'tfidock6@51.la',
+    password: 'FHBlm0'
+  },
+  {
+    id: 8,
+    username: 'lcrittal7',
+    email: 'hoshee7@hatena.ne.jp',
+    password: 'UH8ejecg4'
+  },
+  {
+    id: 9,
+    username: 'lmcreynolds8',
+    email: 'tedlestone8@wunderground.com',
+    password: 'LfBDrBh'
+  },
+  {
+    id: 10,
+    username: 'skestin9',
+    email: 'econti9@scientificamerican.com',
+    password: 'g8bQiQdyhju2'
+  },
+  {
+    id: 11,
+    username: 'acalfa',
+    email: 'saubrya@accuweather.com',
+    password: 'QbOAuo1T7'
+  },
+  {
+    id: 12,
+    username: 'bcicconettiib',
+    email: 'abartoszewskib@unicef.org',
+    password: 'SdEoMOuox45'
+  },
+  {
+    id: 13,
+    username: 'khadleyc',
+    email: 'amehewc@discovery.com',
+    password: 'EtlTCL5ncDA'
+  },
+  {
+    id: 14,
+    username: 'jslaterd',
+    email: 'achallesd@1und1.de',
+    password: 'sC02LHd'
+  },
+  {
+    id: 15,
+    username: 'egazee',
+    email: 'myallope@fda.gov',
+    password: 'LWEyFf4KWWnS'
+  },
+  {
+    id: 16,
+    username: 'idurtnallf',
+    email: 'jpremblef@apple.com',
+    password: 'wHo1XQoQw0k'
+  },
+  {
+    id: 17,
+    username: 'ctydemang',
+    email: 'lguildfordg@weibo.com',
+    password: 'QXsqFDZa5Fm'
+  },
+  {
+    id: 18,
+    username: 'rlogesdaleh',
+    email: 'evillaronh@nps.gov',
+    password: 'fz9cHfc'
+  },
+  {
+    id: 19,
+    username: 'bwincklesi',
+    email: 'aurweni@wsj.com',
+    password: 'l3pQlGS3BP0b'
+  },
+  {
+    id: 20,
+    username: 'tlewerj',
+    email: 'bpickringj@yahoo.co.jp',
+    password: 'eeBrdp'
+  },
+  {
+    id: 21,
+    username: 'kkrugk',
+    email: 'hbabek@etsy.com',
+    password: '5gkW2JKq'
+  },
+  {
+    id: 22,
+    username: 'ddalglieshl',
+    email: 'searlyl@wordpress.com',
+    password: 'EMV7xEYc'
+  },
+  {
+    id: 23,
+    username: 'gmcneighm',
+    email: 'blanonm@a8.net',
+    password: 'VLQNTL'
+  },
+  {
+    id: 24,
+    username: 'apriestnalln',
+    email: 'delgeen@berkeley.edu',
+    password: '0mCMwh8'
+  },
+  {
+    id: 25,
+    username: 'zlokiero',
+    email: 'hconneauo@seattletimes.com',
+    password: 'QKXEgvr1LR'
+  },
+  {
+    id: 26,
+    username: 'kridlerp',
+    email: 'ehasselbyp@home.pl',
+    password: 'V3LoNwl'
+  },
+  {
+    id: 27,
+    username: 'gramelotq',
+    email: 'dratleyq@geocities.jp',
+    password: 'PeVOnb8wqjsc'
+  },
+  {
+    id: 28,
+    username: 'egillhamr',
+    email: 'sbaldinr@facebook.com',
+    password: 'MZg6nnXNL7'
+  },
+  {
+    id: 29,
+    username: 'hyeulets',
+    email: 'tjarrells@bizjournals.com',
+    password: 'KNnKlIm'
+  },
+  {
+    id: 30,
+    username: 'rcritzent',
+    email: 'vbrockestt@google.de',
+    password: '7pG3WXJA'
+  },
+  {
+    id: 31,
+    username: 'moffordu',
+    email: 'swillarsu@apple.com',
+    password: 'NlMHPU5UqD7g'
+  },
+  {
+    id: 32,
+    username: 'ltancockv',
+    email: 'akrysztowczykv@mozilla.org',
+    password: 'CNPLENIugs0A'
+  },
+  {
+    id: 33,
+    username: 'ypauncefootw',
+    email: 'rwitnallw@biblegateway.com',
+    password: 'FQ3SF6cws8E'
+  },
+  {
+    id: 34,
+    username: 'agarmstonx',
+    email: 'reasenx@yolasite.com',
+    password: 'QxBhKr'
+  },
+  {
+    id: 35,
+    username: 'mlacky',
+    email: 'mdigiorgioy@shutterfly.com',
+    password: 'vM6ayA3zZc'
+  },
+  {
+    id: 36,
+    username: 'eolfertz',
+    email: 'sbiertonz@yahoo.co.jp',
+    password: 'tuheSpv0oBE'
+  },
+  {
+    id: 37,
+    username: 'nrankcom10',
+    email: 'rmapston10@exblog.jp',
+    password: 'ZSpao2Zy'
+  },
+  {
+    id: 38,
+    username: 'shedden11',
+    email: 'istirling11@slideshare.net',
+    password: 'ajs7hZ'
+  },
+  {
+    id: 39,
+    username: 'jlaurenz12',
+    email: 'smacranald12@cnet.com',
+    password: 'b85UpCUo'
+  },
+  {
+    id: 40,
+    username: 'ndimmer13',
+    email: 'jpennings13@lycos.com',
+    password: '01WmRCErl9cQ'
+  },
+  {
+    id: 41,
+    username: 'hnovill14',
+    email: 'achiechio14@mysql.com',
+    password: 'i5Fq74U98Z'
+  },
+  {
+    id: 42,
+    username: 'vpriestland15',
+    email: 'aczaja15@google.com.hk',
+    password: 'D625gbfqbM'
+  },
+  {
+    id: 43,
+    username: 'losmant16',
+    email: 'lklink16@themeforest.net',
+    password: 'TJY3Ur0JrVNj'
+  },
+  {
+    id: 44,
+    username: 'akeeney17',
+    email: 'swalbridge17@businessweek.com',
+    password: 'drivsp'
+  },
+  {
+    id: 45,
+    username: 'vmoseley18',
+    email: 'jchetwind18@biglobe.ne.jp',
+    password: '5b6CKb'
+  },
+  {
+    id: 46,
+    username: 'eaveling19',
+    email: 'mbuddington19@elegantthemes.com',
+    password: 'QSvaOOVY6683'
+  },
+  {
+    id: 47,
+    username: 'cbaud1a',
+    email: 'ekittle1a@wikimedia.org',
+    password: 'kWgbnRvvSs'
+  },
+  {
+    id: 48,
+    username: 'emccreath1b',
+    email: 'bausher1b@trellian.com',
+    password: 'PeMZwy0mB8'
+  },
+  {
+    id: 49,
+    username: 'wlimbourne1c',
+    email: 'bbeldan1c@yahoo.com',
+    password: 'GfJoVygjLEk'
+  },
+  {
+    id: 50,
+    username: 'kshank1d',
+    email: 'esealeaf1d@amazon.co.jp',
+    password: 'f91eNA32d8Tg'
+  },
+  {
+    id: 51,
+    username: 'wdaintrey1e',
+    email: 'wcaldwall1e@techcrunch.com',
+    password: 'c3McqQnAZ2'
+  },
+  {
+    id: 52,
+    username: 'scastelin1f',
+    email: 'poutlaw1f@weibo.com',
+    password: 'xp4G70JV'
+  },
+  {
+    id: 53,
+    username: 'hcapener1g',
+    email: 'hkeetley1g@symantec.com',
+    password: 'ohexpX'
+  },
+  {
+    id: 54,
+    username: 'kdurtnal1h',
+    email: 'ldawid1h@smh.com.au',
+    password: 'wjh8LIf'
+  },
+  {
+    id: 55,
+    username: 'tbertouloume1i',
+    email: 'sberrie1i@bing.com',
+    password: '6X1F1a'
+  },
+  {
+    id: 56,
+    username: 'ipett1j',
+    email: 'ablakelock1j@dropbox.com',
+    password: '4peJdE'
+  },
+  {
+    id: 57,
+    username: 'acrossan1k',
+    email: 'elukesch1k@typepad.com',
+    password: 'ftTTzUu'
+  },
+  {
+    id: 58,
+    username: 'fdoe1l',
+    email: 'mokennedy1l@mapquest.com',
+    password: 'Y9qaPqnXCPYp'
+  },
+  {
+    id: 59,
+    username: 'ddonlon1m',
+    email: 'abrolechan1m@jalbum.net',
+    password: 'AU5M24WgIg'
+  },
+  {
+    id: 60,
+    username: 'dstocken1n',
+    email: 'cgarett1n@unicef.org',
+    password: 'elpHmaPk'
+  },
+  {
+    id: 61,
+    username: 'gmacnaughton1o',
+    email: 'wchipping1o@netscape.com',
+    password: 'O0ew7wQg'
+  },
+  {
+    id: 62,
+    username: 'fbritcher1p',
+    email: 'bllewellin1p@ameblo.jp',
+    password: 'XDld07dYS'
+  },
+  {
+    id: 63,
+    username: 'sbennell1q',
+    email: 'hphebey1q@live.com',
+    password: 'S6Q4FyuzNb6'
+  },
+  {
+    id: 64,
+    username: 'rspolton1r',
+    email: 'mcota1r@salon.com',
+    password: 'byLGaE'
+  },
+  {
+    id: 65,
+    username: 'kwenger1s',
+    email: 'spoizer1s@51.la',
+    password: 'R3Fy1hZ2On'
+  },
+  {
+    id: 66,
+    username: 'mbonwick1t',
+    email: 'ktithacott1t@slate.com',
+    password: 'FkbOFRN'
+  },
+  {
+    id: 67,
+    username: 'zkeaveny1u',
+    email: 'rside1u@sfgate.com',
+    password: 'TbpM89'
+  },
+  {
+    id: 68,
+    username: 'rlowrey1v',
+    email: 'corpwood1v@usatoday.com',
+    password: 't30AG8HJv'
+  },
+  {
+    id: 69,
+    username: 'mizakoff1w',
+    email: 'sstretton1w@xing.com',
+    password: 'Dbss2FZ61kWB'
+  },
+  {
+    id: 70,
+    username: 'jmoxon1x',
+    email: 'etreversh1x@biglobe.ne.jp',
+    password: 'Z1VNK7r'
+  },
+  {
+    id: 71,
+    username: 'ghalpin1y',
+    email: 'dmedeway1y@cocolog-nifty.com',
+    password: 'ereaZa'
+  },
+  {
+    id: 72,
+    username: 'mwrathmall1z',
+    email: 'rplowright1z@wufoo.com',
+    password: 'lDQ3PNTi1CL'
+  },
+  {
+    id: 73,
+    username: 'cdrakeley20',
+    email: 'brodinger20@gmpg.org',
+    password: 'FkylLGWPRQde'
+  },
+  {
+    id: 74,
+    username: 'fferrarello21',
+    email: 'zokielt21@ted.com',
+    password: 'JBaxEP4v'
+  },
+  {
+    id: 75,
+    username: 'rwoodison22',
+    email: 'lcartlidge22@msu.edu',
+    password: 'juKTZhy0Ykk8'
+  },
+  {
+    id: 76,
+    username: 'echatburn23',
+    email: 'emuscat23@quantcast.com',
+    password: 'j7O0GkM'
+  },
+  {
+    id: 77,
+    username: 'rapplebee24',
+    email: 'gbifield24@hubpages.com',
+    password: 'lHFf6W6fI'
+  },
+  {
+    id: 78,
+    username: 'lhowerd25',
+    email: 'bjurs25@google.com.au',
+    password: 'JTegoo'
+  },
+  {
+    id: 79,
+    username: 'mfleay26',
+    email: 'dtranter26@multiply.com',
+    password: 'zsHuWtEqXkxH'
+  },
+  {
+    id: 80,
+    username: 'amizzen27',
+    email: 'mwinterbourne27@livejournal.com',
+    password: '0sJOCK3'
+  },
+  {
+    id: 81,
+    username: 'ltrythall28',
+    email: 'ckeerl28@so-net.ne.jp',
+    password: 'F4qnF5Qv5'
+  },
+  {
+    id: 82,
+    username: 'bsowerby29',
+    email: 'sdeangelis29@admin.ch',
+    password: 'qcKraegX'
+  },
+  {
+    id: 83,
+    username: 'mgreatrex2a',
+    email: 'czoephel2a@ezinearticles.com',
+    password: 'E1bm9u'
+  },
+  {
+    id: 84,
+    username: 'mroydon2b',
+    email: 'kswayton2b@indiegogo.com',
+    password: 'Se4DsImow2c'
+  },
+  {
+    id: 85,
+    username: 'lzuppa2c',
+    email: 'ckoop2c@storify.com',
+    password: 'JtFRdJootXIh'
+  },
+  {
+    id: 86,
+    username: 'khusselbee2d',
+    email: 'lferrers2d@blogger.com',
+    password: 'oR3LGbZe'
+  },
+  {
+    id: 87,
+    username: 'fganders2e',
+    email: 'rdupoy2e@imdb.com',
+    password: 'xaVwz8BtoBfj'
+  },
+  {
+    id: 88,
+    username: 'ahugland2f',
+    email: 'lgarrick2f@marketwatch.com',
+    password: '8AixmrUcD'
+  },
+  {
+    id: 89,
+    username: 'eollarenshaw2g',
+    email: 'squince2g@jiathis.com',
+    password: 'P4pwCyldm'
+  },
+  {
+    id: 90,
+    username: 'kleport2h',
+    email: 'wstorcke2h@sciencedaily.com',
+    password: 'b1fKuYnilerH'
+  },
+  {
+    id: 91,
+    username: 'cteasdale2i',
+    email: 'cgiraudou2i@trellian.com',
+    password: 'AEjfBecGls3'
+  },
+  {
+    id: 92,
+    username: 'morbon2j',
+    email: 'ccrittal2j@bloglovin.com',
+    password: 'Cc78ybbfGq'
+  },
+  {
+    id: 93,
+    username: 'dibbotson2k',
+    email: 'nmacdermott2k@msn.com',
+    password: 'Ti56hWHXw'
+  },
+  {
+    id: 94,
+    username: 'asammonds2l',
+    email: 'prosevear2l@dropbox.com',
+    password: 'yUfIp2'
+  },
+  {
+    id: 95,
+    username: 'egulc2m',
+    email: 'bbentzen2m@howstuffworks.com',
+    password: 'ObMw9Fbj0Uh'
+  },
+  {
+    id: 96,
+    username: 'dhulland2n',
+    email: 'pmartinho2n@vk.com',
+    password: 'u8btAQPW'
+  },
+  {
+    id: 97,
+    username: 'hhelsby2o',
+    email: 'ismaling2o@123-reg.co.uk',
+    password: 'hryAlntTh'
+  },
+  {
+    id: 98,
+    username: 'tbeston2p',
+    email: 'cmacmeanma2p@infoseek.co.jp',
+    password: 'lIqspaZR'
+  },
+  {
+    id: 99,
+    username: 'apengilly2q',
+    email: 'mmartineau2q@toplist.cz',
+    password: 'm9WeZPj2TASw'
+  },
+  {
+    id: 100,
+    username: 'bverbrugge2r',
+    email: 'bcowndley2r@virginia.edu',
+    password: 'CHjw5rmvKS'
+  }
+];
+
+module.exports = { usersMock };
