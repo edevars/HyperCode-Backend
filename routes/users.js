f21bd037af9c65ade6f6ce72385563d329290ee1
edevars <e.devars.dev@gmail.com>
2019-10-20 18:24:38 -0500

    :sparkles: Implementando CRUD de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,89 @@
 const express = require('express');
-//Hacemos uso de un archivo de mocks, los mocks son datos falsos
+
 const { usersMock } = require('../utils/mocks/users');
 
-//Definimos una funcion userApi para poder crear una ruta
-//en nuestra app de express, por eso como parametro tiene app
 
-//Esto lo hacemos para ser dinamicos y establecer que aplicacion de express
-//puede hacer uso de nuestra ruta
 function usersApi(app) {
-  //creamos un router con express
+  
   const router = express.Router();
 
-  //Definimos la ruta principal donde se consumira nuestra api
   app.use('/api/users', router);
 
-  //Le decimos al router que haga uso de nuestra ruta principal
-  //cuandp se le haga una peticion GET
-
-  //la ruta que se define es '/' pero hace referencia a '/api/users'
   router.get('/', async function(req, res, next) {
     try {
-      //Definimos unos usuarios falsos con nuestro mock
-      //Como las peticiones son asincronas hacemos uso de una promesa
-      //esto hace la simulacion como si nos conectaramos a una base de datos
+
       const users = await Promise.resolve(usersMock);
 
-      //Devolvemos un codigo 200 http que siginifica ok
       res.status(200).json({
-        //devolcemos como datos a nuestros usuarios
         data: users,
-        //devolvemos un mensaje para el cliente
         message: 'Usuarios listados'
       });
     } catch (error) {
-      //Si ocurre un error lo podemos manejar con el parametro next
       next(error);
     }
   });
+
+  //Obtenemos solo un usuario dependiendo su id con el metodo GET y el query param userId
+  router.get('/:userId', async function(req, res, next) {
+    try {
+
+      const user = await Promise.resolve(usersMock[0]);
+
+      res.status(200).json({
+        data: user,
+        message: 'Usuario listado'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+   //Creamos un usuario con el metodo post
+  router.post('/', async function(req, res, next) {
+    try {
+
+      const createdUserId = await Promise.resolve(usersMock[0].id);
+
+      res.status(201).json({
+        data: createdUserId,
+        message: 'Usuario creado'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Actualizamos un usuario por su id con el metodo PUT
+  router.put('/:userId', async function(req, res, next) {
+    try {
+
+      const updatedUserId = await Promise.resolve(usersMock[0].id);
+
+      res.status(200).json({
+        data: updatedUserId,
+        message: 'Usuario actualizado'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Actualizamos un usuario por su id con el metodo PUT
+  router.delete('/:userId', async function(req, res, next) {
+    try {
+
+      const deletedUserId = await Promise.resolve(usersMock[0].id);
+
+      res.status(200).json({
+        data: deletedUserId,
+        message: 'Usuario eliminado'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+ 
 }
 
 module.exports = usersApi;
